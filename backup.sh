bf83cec1c16baab45ccd11ea7aa77f707306e473
Andrew <andrii.kisel@gmail.com>
2021-06-11 15:25:39 +0300

    Update backup.sh
    Added additional information for steps

--- backup.sh
+++ backup.sh
@@ -16,6 +16,8 @@ if [[ "$INPUT_SPACE_ACCESS_KEY_ID" = "" || "$INPUT_SPACE_SECRET_ACCESS_KEY" = ""
   exit 1
 fi
 
+echo "Credentials provided"
+
 touch ~/.s3cfg
 
 echo "[default]
@@ -34,22 +36,30 @@ git clone https://github.com/s3tools/s3cmd
 #----------------------------------------
 # Prepare to recipe to backup
 #----------------------------------------
-
+echo "Generating folder for backup"
 if [ ! -d ./$BACKUP_DIR/ ]; then
     mkdir -p $BACKUP_DIR
 fi
+
+echo "Checking provided credentials for database server"
 
 if [[ "$INPUT_DB_USER" = "" || "$INPUT_DB_NAME" = "" ]]; then
   echo 'db_user and db_name should not be empty, Please specify.'
   exit 1
 fi
 
+echo "Credentials for database server provided"
+
 if [ "$INPUT_DB_TYPE" = "postgres" ]; then
   FILENAME=$INPUT_DB_TYPE-$INPUT_DB_NAME.$CURRENTDATE.pgsql.gz
   INPUT_DB_PORT="${INPUT_DB_PORT:-5432}"
   INPUT_ARGS="${INPUT_ARGS} -C --column-inserts"
   export PGPASSWORD="$INPUT_DB_PASS"
+  echo "Creating database dump"
   pg_dump -U $INPUT_DB_USER -h $INPUT_DB_HOST -p $INPUT_DB_PORT $INPUT_ARGS $INPUT_DB_NAME | gzip -9 > $BACKUP_DIR/$FILENAME
   echo "Db dump completed. File: $FILENAME"
+  echo "Starting upload to DigitalOcean space"
   python3 ./s3cmd/s3cmd put $BACKUP_DIR/$FILENAME s3://$INPUT_SPACE_NAME/$BACKUP_DIR/$FILENAME
 fi
+
+echo "Upload to DigitalOcean space completed"
