97261d078209383316c19f1cfcedf767b60585b4
Odmincheg <andrii.kisel@gmail.com>
2021-06-11 19:10:40 +0300

    fixed backup script

--- backup.sh
+++ backup.sh
@@ -10,32 +10,8 @@ INPUT_PASS=""
 EXTRA_SCRIPT=""
 
 #----------------------------------------
-# DigitalOcean space section
-#----------------------------------------
-echo "Credentials provided"
-
-touch ~/.s3cfg
-
-echo "[default]
-access_key=$INPUT_SPACE_ACCESS_KEY_ID
-secret_key=$INPUT_SPACE_SECRET_ACCESS_KEY
-bucket_location=US
-host_base=$INPUT_SPACE_REGION.digitaloceanspaces.com
-host_bucket=$INPUT_SPACE_NAME.$INPUT_SPACE_REGION.digitaloceanspaces.com
-proxy_host =
-proxy_port = 0
-use_http_expect = False
-use_https = True
-server_side_encryption = False" > ~/.s3cfg
-git clone https://github.com/s3tools/s3cmd
-
-#----------------------------------------
 # Prepare to recipe to backup
 #----------------------------------------
-echo "Generating folder for backup"
-if [ ! -d ./$BACKUP_DIR/ ]; then
-    mkdir -p $BACKUP_DIR
-fi
 
 if [ "$INPUT_DB_ACTION" = "backup" ]; then
   echo "Checking provided credentials for database server"
@@ -45,9 +21,33 @@ if [ "$INPUT_DB_ACTION" = "backup" ]; then
   fi
   echo "Credentials for database server provided"
 
+  #----------------------------------------
+  # DigitalOcean space section
+  #----------------------------------------
+  touch ~/.s3cfg
+
+  echo "[default]
+  access_key=$INPUT_SPACE_ACCESS_KEY_ID
+  secret_key=$INPUT_SPACE_SECRET_ACCESS_KEY
+  bucket_location=US
+  host_base=$INPUT_SPACE_REGION.digitaloceanspaces.com
+  host_bucket=$INPUT_SPACE_NAME.$INPUT_SPACE_REGION.digitaloceanspaces.com
+  proxy_host =
+  proxy_port = 0
+  use_http_expect = False
+  use_https = True
+  server_side_encryption = False" > ~/.s3cfg
+  git clone https://github.com/s3tools/s3cmd
+
   if [[ "$INPUT_SPACE_ACCESS_KEY_ID" = "" || "$INPUT_SPACE_SECRET_ACCESS_KEY" = "" || "$INPUT_SPACE_NAME" = "" ]]; then
     echo 'space_access_key_id and space_secret_access_key and space_name should not be empty, Please specify.'
     exit 1
+  fi
+  echo "API Credentials provided"
+  
+  echo "Generating folder for backup"
+  if [ ! -d ./$BACKUP_DIR/ ]; then
+      mkdir -p $BACKUP_DIR
   fi
   
   if [ "$INPUT_DB_TYPE" = "postgres" ]; then
